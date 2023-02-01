df88be1e5dda3b2e8d41231c26999bbd57ec4c7b
grithaq <afifnurazizi@gmail.com>
2023-02-01 23:51:08 +0800

    CU-860pt4vdw - As Admn I will Be able to view list of category

--- src/blueprint/item_blueprint.py
+++ src/blueprint/item_blueprint.py
@@ -5,6 +5,12 @@ from extensions import db
 
 item_blueprint = Blueprint("item_blueprint", __name__)
 
+
+@item_blueprint.route('/category/')
+def list_category():
+    categories = Category.query.all()
+    return render_template("category.html", categories=categories)
+
 @item_blueprint.route('/category/add', methods=['GET', 'POST'])
 def add_category():
     form = CategoryForm()
@@ -14,5 +20,5 @@ def add_category():
             category.name = form.name.data
             db.session.add(category)
             db.session.commit()
-            return redirect(url_for("item_blueprint.add_category"))
+            return redirect(url_for("item_blueprint.list_category"))
     return render_template("add_category.html", form=form)
