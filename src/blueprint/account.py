1b98c3afcbab398bad0e95afd76fe3f6eba2b9c5
grithaq <afifnurazizi@gmail.com>
2023-02-05 22:54:19 +0800

    CU-860pugkeh - As Admin I will able to update customer

--- src/blueprint/account.py
+++ src/blueprint/account.py
@@ -27,6 +27,25 @@ def add_customer():
             return redirect(url_for('customer_blueprint.list_customer'))
     return render_template("add_customer.html", form=form)
 
+@customer_blueprint.route("/customer/<id>/edit", methods=['GET', 'POST'])
+def edit_customer(id):
+    form = CustomerForm()
+    customer = Customer.query.filter_by(id=id).first()
+    if request.method=='POST':
+        if form.validate_on_submit:
+            customer.first_name = form.first_name.data
+            customer.last_name = form.last_name.data
+            customer.address = form.address.data
+            customer.phone_number = form.phone_number.data
+            db.session.add(customer)
+            db.session.commit()
+            return redirect(url_for('customer_blueprint.list_customer'))
+    form.first_name.data = customer.first_name
+    form.last_name.data = customer.last_name
+    form.address.data = customer.address
+    form.phone_number.data = customer.phone_number
+    return render_template("edit_customer.html", form=form, id=id)
+
 @customer_blueprint.route("/customer/<id>/delete")
 def delete_customer(id):
     customer = Customer.query.filter_by(id=id).first()
